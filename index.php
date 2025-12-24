64e5df6cb56fbcb07f85252acf5222bdd4955af4
iXeo <ixeo2@yandex.ru>
2025-12-24 09:24:46 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">        
+    </head>
+    <body>
+<!--container-->
+        <div class="container">
+<!--header-->
+            <header></header>
+<!--nav-->            
+            <nav></nav>
+<!--main-->
+            <main></main>
+<!--sidebar-->
+            <sidebar></sidebar>
+<!--footer-->            
+            <footer></footer>
+        </div>
+    </body>
+</html>
