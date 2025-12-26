9b762d071b791012ae66c09218aa83cfafed8dad
iXeo <ixeo2@yandex.ru>
2025-12-26 15:24:41 +0300

    feat: add homepage with vertical sections

--- index.php
+++ index.php
@@ -3,21 +3,31 @@
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">        
+    <link rel="stylesheet" href="CSS/style.css">        
     </head>
     <body>
-<!--container-->
+
+<!-- Home section -->        
+    <section id="home" class="section home-section">
         <div class="container">
-<!--header-->
-            <header></header>
-<!--nav-->            
-            <nav></nav>
-<!--main-->
-            <main></main>
-<!--sidebar-->
-            <sidebar></sidebar>
-<!--footer-->            
-            <footer></footer>
+            <h1>PORTFOLIO</h1>
+            <footer>
+                <div class="footer_part">
+                    <p>2025</p>
+                    <p>Xeo</p>
+                </div>  
+            </footer>
         </div>
+    </section>
+<!-- About section -->   
+    <section id="about" class="section about-section">
+        2
+    </section>
+<!-- Contact section --> 
+    <section id="contact" class="section contact-section">
+        3
+    </section>
+
+<script src="script.js"></script>
     </body>
 </html>
